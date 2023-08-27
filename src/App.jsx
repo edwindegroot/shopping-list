b117272910fb66c9c8e202c8002f23865bcc2752
Edwin de Groot <edwindegroot93@gmail.com.com>
2023-08-27 16:42:12 +0200

    Finish first version of simple shopping list

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ let baseItems = [
 function App() {
   const [name, setName] = useState('')
   const [items, setItems] = useState(baseItems)
+  const [favourites, setFavourites] = useState([])
 
   const handleChange = (e) => {
     setName(e.target.value)
@@ -16,23 +17,54 @@ function App() {
 
   const Item = ({item}) => {
     return(
-      <div>{item}</div>
+      <div>{item} <button onClick={() => remove(item)}>x</button> <button onClick={() => addFavourite(item)}>Favourite</button> </div>
     )
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    if (items.includes(name)) {
-      alert(`${name} is already in the list`)
+  const addFavourite = (item) => {
+    let copy = [...favourites]
+    copy.push(item)
+    setFavourites(copy)
+  }
+
+  const Favourite = ({item}) => {
+    return(
+      <div>{item} <button onClick={() => removeFavourite(item)}>x</button> <button onClick={() => addItem(item)}>Add to shopping list</button></div>
+    )
+  }
+
+  const addItem = (item) => {
+    if (items.includes(item)) {
+      alert(`${item} is already in the list`)
       return
     }
     let itemsCopy = [...items]
-    itemsCopy.push(name)
+    itemsCopy.push(item)
+    setName('')
     setItems(itemsCopy)
   }
 
   const remove = (toBeRemoved) => {
-    
+    if (!window.confirm(`Remove ${toBeRemoved} from shopping list?`)) return
+    let copy = [...items]
+    setItems(copy.filter(item => item !== toBeRemoved))
+  }
+
+  const removeFavourite = (toBeRemoved) => {
+    if (!window.confirm(`Remove ${toBeRemoved} from favourites?`)) return
+    let copy = [...favourites]
+    setFavourites(copy.filter(item => item !== toBeRemoved))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addItem(name)
+  }
+
+  const clearList = () => {
+    if (window.confirm("Clear shopping list?")) {
+      setItems([])
+    }
   }
 
   return (
@@ -40,6 +72,10 @@ function App() {
       <h1>Shopping list</h1>
       <Form name={name} changeFunction={handleChange} handleSubmit={handleSubmit}/>
       {items.map((item, id) => <Item key={id} item={item}/>)}
+      <button onClick={() => clearList()}>Clear</button>
+      <h2>Favourites</h2>
+      {favourites.map((item, id) => <Favourite key={id} item={item}/>)}
+
     </>
   )
 }
